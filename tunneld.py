23bdb3f100be1650f97bdab799a5ba4beaee9adb
Faizal Sadri <faizal@geekdo.me>
2023-10-20 01:44:41 +0800

    remove pid file

--- tunneld.py
+++ tunneld.py
@@ -18,25 +18,9 @@ def start_tunnel(tunnel_command):
     tunnel_name = os.path.basename(tunnel_command[2])
     sanitized_tunnel_name = sanitize_filename(tunnel_name)
 
-    pid_file = os.path.join('run', f"{sanitized_tunnel_name}.pid")
-
-    if os.path.exists(pid_file):
-        with open(pid_file, 'r') as pid_file:
-            existing_pid = int(pid_file.read())
-        if is_process_running(existing_pid):
-            print(f"Tunnel for {tunnel_name} is already running (PID: {existing_pid}).")
-            return
-        else:
-            # Cleanup (delete) the old PID file if the process is not running
-            os.remove(pid_file.name)
-
     # Start the tunnel in the background
     process = subprocess.Popen(tunnel_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, close_fds=True)
-    print("Tunnel started.")
-
-    # Write the PID to the specified file
-    with open(pid_file.name, 'w') as pid_file:
-        pid_file.write(str(process.pid))
+    print(f"Tunnel started. {sanitized_tunnel_name}")
 
 def main():
     # Define tunnel configurations and PID file names
